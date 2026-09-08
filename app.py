8091c5da183c5db038bf23eac33144f39b3ddacf
Ashutosh-Kumar-Dubey <ashutoshdubeykashyap@gmail.com>
2026-09-08 19:15:20 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("""
 # --- BACKEND LOGIC ---
 @st.cache_resource
 def get_inngest_client() -> inngest.Inngest:
-    return inngest.Inngest(app_id="rag_app", is_production=False)
+    return inngest.Inngest(app_id="rag_app", is_production=os.getenv('INNGEST_EVENT_KEY') is not None)
 
 
 def save_uploaded_pdf(file) -> Path:
